0f7645f59c5bee2e61cd2911128ef7df1766c9d0
Yasmim Freitas <yasmimfreitas0502@gmail.com>
2025-07-13 00:36:20 -0300

    Colocando funções e opçoes de calculo

--- calculadora.py
+++ calculadora.py
@@ -1,2 +1,20 @@
+def somar (a, b):
+    return a + b
 
+def subtrair (a, b):
+    return a - b
+
+def multiplicar (a, b):
+    return a * b
+
+def dividir (a, b):
+    if b == 0:
+        return 'Erro: Divisão por zero!'
+    return a / b
+
+print('Calculadora em Python')
+print('1 - Somar')
+print('2 - Subtrair')
+print('3 - Multiplicar')
+print('4 - Divisão')
 
